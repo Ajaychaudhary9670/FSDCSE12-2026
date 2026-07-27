84243a8bef540791569198b42a21686b2a831686
Ajaychaudhary9670 <ajajaychaudhary1616@gmail.com>
2026-07-27 09:45:09 +0530

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+
+console.log("Hello, JavaScript!");
+
+let name = "Ajay";
+let age = 20;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+// Function
+function greet(name) {
+    return `Hello, ${name}! Welcome to JavaScript.`;
+}
+
+console.log(greet(name));
